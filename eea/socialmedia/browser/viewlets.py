e8b6c0ec9e35733553b3e8aa6437359f8cee4050
prospchr <prospchr@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-04-20 14:11:23 +0000

    refs #9079 [JENKINS] - Fix pylint warnings

--- eea/socialmedia/browser/viewlets.py
+++ eea/socialmedia/browser/viewlets.py
@@ -1,9 +1,7 @@
 """ Custom viewlets
 """
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets import common, content
-from Products.CMFCore.utils import getToolByName
-
+from plone.app.layout.viewlets import content
 
 class SocialMediaViewlet(content.DocumentActionsViewlet):
     """A modified footer viewlet to contain social media icons
