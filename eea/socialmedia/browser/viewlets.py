0a3b6deb34f987f0e3c6294ba8303a4068794e2a
prospchr <prospchr@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-05-21 12:52:00 +0000

    Change: Change code in eea.socialmedia to fix translation problem [prospchr refs #9144]

--- eea/socialmedia/browser/viewlets.py
+++ eea/socialmedia/browser/viewlets.py
@@ -2,11 +2,42 @@
 """
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets import content
+from Products.CMFCore.utils import getToolByName
+from Acquisition import aq_inner
 
 class SocialMediaViewlet(content.DocumentActionsViewlet):
     """A modified footer viewlet to contain social media icons
     """
     render = ViewPageTemplateFile('socialmedia.pt')
+    
+    def create_locale(self):
+        """There is no way to guess the locale, so we do it by hand.
+           Few countries have a different code than their language,
+           so we check them case by case
+        """
+        context = aq_inner(self.context)
+        plt = getToolByName(context, 'portal_languages')
+        pl = plt.getPreferredLanguage()
+        
+        if pl == "cs":
+            new_locale = "cs_CZ"
+        elif pl == "da":
+            new_locale = "da_DK"
+        elif pl == "el":
+            new_locale = "el_GR"
+        elif pl == "en":
+            new_locale = "en_GB"
+        elif pl == "et":
+            new_locale = "et_EE"
+        elif pl == "sl":
+            new_locale = "sl_SI"
+        elif pl == "sv":
+            new_locale = "sv_SE"            
+        else:
+            new_locale = pl + "_" + pl.upper()
+        
+        return new_locale
+    
     
         
 
