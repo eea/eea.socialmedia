04d5d5487019c9dad749e16e4cbcb91eaab1b807
robaaoli <robaaoli@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-01-07 14:24:39 +0000

    Added ISocialMediaLayer add-on layer

---
+++ eea/socialmedia/browser/interfaces.py
@@ -0,0 +1,8 @@
+""" Browser interfaces
+"""
+from zope.interface import Interface
+
+
+class ISocialMediaLayer(Interface):
+    """ Custom layer for this package
+    """
---
+++ eea/socialmedia/interfaces.py
@@ -0,0 +1,8 @@
+""" Public interfaces
+"""
+# Browser layer
+from eea.socialmedia.browser.interfaces import ISocialMediaLayer
+
+__all__ = [
+    ISocialMediaLayer.__name__,
+]
